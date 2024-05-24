bd701349da6f11200008740ac472cc1987a3ea5a
AdamFariello <ajf270@scarletmail.rutgers.edu>
2024-05-24 01:54:09 -0400

    Laying bare bones of algorithm

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,9 +1,21 @@
 public class Algorithm {
+    int count, swap;
+
+
+    private void initAlgorithm () { 
+        count = swap = 0; 
+    }
+
+    private boolean check (int a, int b) {
+        
+    }
 
     private void swap (Object a, Object b) {
 
     }
 
 
+    static public void bubblesort(int[] arr) {
 
+    }
 }
