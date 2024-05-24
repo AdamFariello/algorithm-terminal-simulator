aeed302c2b2fd9f1990fb0ce91ed94e416fd6a21
AdamFariello <ajf270@scarletmail.rutgers.edu>
2024-05-24 03:11:13 -0400

    Added lambda timer function to Algorithm class, currently debugging

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -2,51 +2,79 @@ import java.lang.reflect.Method;
 
 @FunctionalInterface 
 interface lambda {
-    void timer(int[] arr, Method m);
+    void run(int[] arr, Method method, Algorithm classInstance);
 }
+
+/*
+//TODO: Look into this as an idea
+//      Would help with making sure algorithm method gets used properly 
+class Algorithsm {
+    abstract class algorithm {
+        abstract void method();
+        abstract void methodCaller();
+    }  
+
+    class bubblesort extends algorithm {
+        void method() {
+
+        }
+
+        void methodCaller () {
+
+        }
+    }
+}
+*/
 
 
 public class Algorithm {
-    int count, swap;
-    long startTime, endTime;
+    static int count, swap;
+    static long startTime, endTime;
 
+    static lambda timer = (arr, method, classInstance) -> {
+        try {
+            long startTime = System.currentTimeMillis();
+            method.invoke(classInstance, arr);
+            long endTime = System.currentTimeMillis();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    };
     
-    private void runningAlgorithm(int [] arr, String methodName) {
+    private static void runningAlgorithm(int [] arr, String methodName) {
         try {
             //Initialize variable
             count = swap = 0;
 
             Class[] parameterTypes = new Class[1];
             parameterTypes[0] = int[].class;
-            Method functionToPass = Algorithm.class.getMethod(methodName, parameterTypes);
-
+            Method methodToPass = Algorithm.class.getMethod(methodName, parameterTypes);
             Algorithm algorithm = new Algorithm();
+
+            //TODO: Get rid of debug print statements
+            timer.run(arr, methodToPass, algorithm);
+            System.out.println("Start: " + startTime);
+            System.out.println("End: " + endTime);
         } catch (Exception e) {
             e.getStackTrace();
         }
     }
 
-
-    private void timeAlgorithm () {
-        long startTime = System.currentTimeMillis();
-        //doReallyLongThing();
-        long endTime = System.currentTimeMillis();
-    }
-
-
     private boolean check (int a, int b) {
-        
-
-
+        //TODO
         return true;
     }
 
     private void swap (Object a, Object b) {
-
+        //TODO
     }
 
 
-    static public void bubblesort(int[] arr) {
-
+    private void bubblesortAlgorithm(int[] arr) {
+        //TODO
+        for (int i = 0; i < 10; i++);
+    }
+    public static void bubblesort(int[] arr) {
+        runningAlgorithm(arr, "bubblesortAlgorithm");
     }
 }
--- src/App.java
+++ src/App.java
@@ -1,7 +1,8 @@
 import java_oop.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+        System.out.println("Class start");
+        Algorithm.bubblesort(null);
     }
 }
