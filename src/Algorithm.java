8999440486cd67eb2f44b69daea4be1bf999dc87
AdamFariello <ajf270@scarletmail.rutgers.edu>
2024-05-24 03:42:29 -0400

    Took commits from first_draft, and cleaned it up for dev branch

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,115 +1,11 @@
 import java.lang.reflect.Method;
 
-@FunctionalInterface 
-interface lambda {
-    void run(int[] arr, Method method, Algorithm classInstance);
-}
-
-//TODO: Look into this as an idea
-//      Would help with making sure algorithm method gets used properly 
-class Algorithsm {
-    @FunctionalInterface 
-    interface lambda {
-        void run(int[] arr, Method method, Algorithm classInstance);
-    }
-
-    abstract class algorithm {
-        static int count, swap;
-        static long startTime, endTime;
-    
-        static lambda timer = (arr, method, classInstance) -> {
-            try {
-                startTime = System.currentTimeMillis();
-                method.invoke(classInstance, arr);
-                endTime = System.currentTimeMillis();
-            } catch (Exception e) {
-                e.getStackTrace();
-            }
-        };
-
-        abstract void swap();
-        abstract boolean check();
-        abstract void method();
-        
-        void methodCaller(int[] arr) {
-            runningAlgorithm(null, "method");
-        }
-        void runningAlgorithm(int [] arr, String methodName) {
-            try {
-                //Initialize variable
-                count = swap = 0;
-    
-                Class[] parameterTypes = new Class[1];
-                parameterTypes[0] = int[].class;
-                Method methodToPass = Algorithm.class.getDeclaredMethod(methodName, parameterTypes);
-                Algorithm algorithm = new Algorithm();
-    
-                //TODO: Get rid of debug print statements
-                timer.run(arr, methodToPass, algorithm);
-                System.out.println("Start: " + startTime);
-                System.out.println("End: " + endTime);
-            } catch (Exception e) {
-                e.getStackTrace();
-            }
-        }
-    }  
-
-    class Bubblesort extends algorithm {
-        void method() {
-            //TODO: Remove
-            for (int i = 0; i < 10; i++);
-        }
-
-        @Override
-        void swap() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'swap'");
-        }
-
-        @Override
-        boolean check() {
-            // TODO Auto-generated method stub
-            throw new UnsupportedOperationException("Unimplemented method 'check'");
-        }
-    }
-}
-
 
 public class Algorithm {
     static int count, swap;
     static long startTime, endTime;
 
-    static lambda timer = (arr, method, classInstance) -> {
-        try {
-            startTime = System.currentTimeMillis();
-            method.invoke(classInstance, arr);
-            endTime = System.currentTimeMillis();
-        } catch (Exception e) {
-            e.getStackTrace();
-        }
-    };
-    
     private static void runningAlgorithm(int [] arr, String methodName) {
-        try {
-            //Initialize variable
-            count = swap = 0;
-
-            Class[] parameterTypes = new Class[1];
-            parameterTypes[0] = int[].class;
-            Method methodToPass = Algorithm.class.getDeclaredMethod(methodName, parameterTypes);
-            Algorithm algorithm = new Algorithm();
-
-            //TODO: Get rid of debug print statements
-            timer.run(arr, methodToPass, algorithm);
-            System.out.println("Start: " + startTime);
-            System.out.println("End: " + endTime);
-        } catch (Exception e) {
-            e.getStackTrace();
-        }
-    }
-
-
-    private static void runningAlgorithm2(int [] arr, String methodName) {
         try {
             //Initialize variable
             count = swap = 0;
@@ -117,7 +13,6 @@ public class Algorithm {
             Method method = Algorithm.class.getDeclaredMethod(methodName, int[].class);
             Algorithm algorithm = new Algorithm();
 
-            System.out.println("Got to here");
             startTime = System.currentTimeMillis();
             method.invoke(algorithm, arr);
             endTime = System.currentTimeMillis();
@@ -146,6 +41,6 @@ public class Algorithm {
     }
     public static void bubblesort(int[] arr) {
         //runningAlgorithm(arr, "bubblesortAlgorithm");
-        Algorithsm.bubblesort();
+        runningAlgorithm(null, "bubblesortAlgorithm");
     }
 }
