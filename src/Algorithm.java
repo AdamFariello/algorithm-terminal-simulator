9adbae2569905aa6e8d7e78d5dabc152c30ebcc5
AdamFariello <ajf270@scarletmail.rutgers.edu>
2024-05-24 02:36:46 -0400

    First step of figuring out how I want to do the timer in algorithm

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,13 +1,44 @@
+import java.lang.reflect.Method;
+
+@FunctionalInterface 
+interface lambda {
+    void timer(int[] arr, Method m);
+}
+
+
 public class Algorithm {
     int count, swap;
+    long startTime, endTime;
 
+    
+    private void runningAlgorithm(int [] arr, String methodName) {
+        try {
+            //Initialize variable
+            count = swap = 0;
 
-    private void initAlgorithm () { 
-        count = swap = 0; 
+            Class[] parameterTypes = new Class[1];
+            parameterTypes[0] = int[].class;
+            Method functionToPass = Algorithm.class.getMethod(methodName, parameterTypes);
+
+            Algorithm algorithm = new Algorithm();
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
     }
+
+
+    private void timeAlgorithm () {
+        long startTime = System.currentTimeMillis();
+        //doReallyLongThing();
+        long endTime = System.currentTimeMillis();
+    }
+
 
     private boolean check (int a, int b) {
         
+
+
+        return true;
     }
 
     private void swap (Object a, Object b) {
